15d588b0c90e9e29bd58b34047cd0b3251ef005d
camposss <ccamposs23@gmail.com>
2018-01-03 13:06:36 -0800

    created the cards with click handlers and flip ability

---
+++ src/assets/helpers/card_data.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import back from '../images/card_back.png';
+import react from '../images/react_logo.svg';
+import angular from '../images/angular_logo.png';
+
+export default [
+    {
+        back: back,
+        flipped: false,
+        front: react
+    },
+    {
+        back: back,
+        flipped: false,
+        front: react
+    },
+    {
+        back: back,
+        flipped: false,
+        front: angular
+    },
+    {
+        back: back,
+        flipped: false,
+        front: angular
+    }
+]
+
--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import '../assets/css/card.css';
+import CardData from '../assets/helpers/card_data';
+import Card from './card';
 
-const App = () => (
-    <div className="app">
-        <h1>Memory Match</h1>
-    </div>
-);
 
+class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state= {
+            cards: CardData
+        };
+        this.flipCard=this.flipCard.bind(this);
+    }
+    flipCard(index){
+        const newCards= this.state.cards.slice();
+        newCards[index].flipped= !newCards[index].flipped;
+        this.setState({
+            cards: newCards
+        });
+    }
+    render(){
+        const cardElements= this.state.cards.map((card,index)=>{
+           return <Card flip ={()=>this.flipCard(index)} card={card} key={index}/>
+
+        });
+        return(
+            <div className="app">
+                {cardElements}
+            </div>
+        )
+    }
+}
 export default App;
