d1db5d2093b3b1c6fbd08a8117aebeff3518b00c
camposss <ccamposs23@gmail.com>
2018-01-03 16:52:57 -0800

    refactored code and game logic. game is working

--- src/components/app.js
+++ src/components/app.js
@@ -14,23 +14,30 @@ class App extends Component{
             cards: CardData,
             matches: 0,
             attempts: 0,
+            gameState: 'ready'
 
         };
         this.flipCard=this.flipCard.bind(this);
+        //if it affects what will be rendered on the screen then it has to go in state
+        //since this will be purely mechanical we can set a new property
+        //if we were to set this as a set, it would run asynchronously and the flag wouldn't apply appropriately.
+        this.blockClick= false;
     }
     handleCardClick(index){
+        if (this.blockClick) return;
         const{firstCardIndex, cards}= this.state;
         let matches= this.state.matches;
         let attempts= this.state.attempts;
+        let cardIndex= null;
+        let gameState= this.state.gameState;
 
         if(firstCardIndex===null){
             console.log('first card clicked');
 
-            this.setState({
-                firstCardIndex: index
-            });
+            cardIndex=index;
             this.flipCard(index);
         }else{
+            this.blockClick= true;
             attempts++;
             console.log('second card clicked');
             const card1= cards[firstCardIndex].front;
@@ -39,26 +46,26 @@ class App extends Component{
             if(card1===card2){
                 matches++;
                 console.log('found a match!!!');
-                this.setState({
-                    firstCardIndex: null,
-                    matches: matches,
-                    attempts: attempts
-                });
                 if(matches ===cards.length/2){
                     console.log('YOU WINNNNNN!');
+                    gameState='won';
                 }
+                this.blockClick=false;
             }else{
                 console.log('the two cards are different :{ ');
                 setTimeout(()=>{
                     this.flipCard(firstCardIndex);
                     this.flipCard(index);
+                    this.blockClick=false;
                 },500);
-                this.setState({
-                    firstCardIndex: null,
-                    attempts: attempts
-                })
             }
         }
+        this.setState({
+            firstCardIndex: cardIndex,
+            matches: matches,
+            attempts: attempts,
+            gameState: gameState
+        });
 
     }
     flipCard(index){
@@ -69,8 +76,7 @@ class App extends Component{
         });
     }
     render(){
-        const {cards, attempts, matches}= this.state;
-
+        const {cards, attempts, matches, gameState}= this.state;
 
         const cardElements= cards.map((card,index)=>{
            return <Card flip ={()=>this.handleCardClick(index)} card={card} key={index}/>
@@ -79,6 +85,7 @@ class App extends Component{
         return(
             <div className="app">
                 <h1>Memory Match with Scott-demy</h1>
+                <h1>{gameState=== 'won' ? 'You won!!!': ''}</h1>
                 <h3>Accuracy: {attempts? ((matches/attempts) * 100).toFixed(2): 0}%</h3>
                 <div className='game-board'>
                     {cardElements}
